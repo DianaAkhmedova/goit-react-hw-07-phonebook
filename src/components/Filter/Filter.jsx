bcc40fcfaea98ed70a80dad30a9319641cf491b6
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-06 19:06:09 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,7 @@
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getFilter } from 'redux/filter/filter-selectors';
+import { setFilter } from 'redux/filter/filter-slice';
 
 import {
   Form,
@@ -6,7 +9,13 @@ import {
   FormInput,
 } from 'components/ContactForm/ContactForm.styled';
 
-const Filter = ({ filterValue, handleChangeFilter }) => {
+const Filter = () => {
+  const filterValue = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const handleChangeFilter = ({ currentTarget: { value } }) =>
+    dispatch(setFilter(value));
+
   return (
     <Form>
       <FormLabel>
@@ -23,8 +32,3 @@ const Filter = ({ filterValue, handleChangeFilter }) => {
   );
 };
 export default Filter;
-
-Filter.propTypes = {
-  filterValue: PropTypes.string.isRequired,
-  handleChangeFilter: PropTypes.func.isRequired,
-};
