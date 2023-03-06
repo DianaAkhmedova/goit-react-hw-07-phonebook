4425cf76285fb9cc57a1e585055e971cf7d4e6b4
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-06 19:06:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
-import {
-  getAllContacts,
-  getFilteredContacts,
-} from '../redux/contacts/contacts-selectors';
-import { getFilter } from 'redux/filter/filter-selectors';
-import { addContact, deleteContact } from 'redux/contacts/contacts-slice';
-import { setFilter } from 'redux/filter/filter-slice';
+import { getFilteredContacts } from '../redux/contacts/contacts-selectors';
 
 import {
   PhonebookContainer,
@@ -19,43 +13,16 @@ import {
 } from './App.styled';
 
 const App = () => {
-  const contacts = useSelector(getAllContacts);
   const filteredContacts = useSelector(getFilteredContacts);
-  const filter = useSelector(getFilter);
-
-  const dispatch = useDispatch();
-
-  const handleAddContact = contact => {
-    const isContainsContact = contacts.some(
-      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
-    );
-
-    if (isContainsContact)
-      return alert(`${contact.name} is already in contacts`);
-
-    dispatch(addContact(contact));
-  };
-
-  const handleChangeFilter = ({ currentTarget: { value } }) =>
-    dispatch(setFilter(value));
-
-  const handleDeleteContact = contactId => {
-    dispatch(deleteContact(contactId));
-  };
 
   return (
     <PhonebookContainer>
       <PhonebookTitle>Phonebook</PhonebookTitle>
-      <ContactForm addContact={handleAddContact} />
+      <ContactForm />
 
       <ContactsTitle>Contacts</ContactsTitle>
-      <Filter filterValue={filter} handleChangeFilter={handleChangeFilter} />
-      {filteredContacts.length > 0 && (
-        <ContactList
-          contacts={filteredContacts}
-          removeContact={handleDeleteContact}
-        />
-      )}
+      <Filter />
+      {filteredContacts.length > 0 && <ContactList />}
     </PhonebookContainer>
   );
 };
