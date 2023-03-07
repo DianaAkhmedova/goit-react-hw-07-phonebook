710f5e9f3d187d6ba65f324a824454e40ff711c4
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-08 00:13:37 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,17 @@
 import { useSelector } from 'react-redux';
 
 import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
-
 import ContactListItem from './ContactListItem/ContactListItem';
 
 const ContactList = () => {
   const contacts = useSelector(getFilteredContacts);
 
-  const contactsItem = contacts.map(({ id, name, number }) => (
+  const contactsItem = contacts.map(({ id, name, phone }) => (
     <ContactListItem
       key={id}
       id={id}
       name={name}
-      number={number}
+      number={phone}
       type="button"
     />
   ));
