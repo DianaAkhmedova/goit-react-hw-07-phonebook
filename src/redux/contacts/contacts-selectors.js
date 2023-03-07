eab6b6854d02fe1a09392b4fcb0303fb53b4cdba
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-08 00:13:51 +0200

    Update contacts-selectors.js

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,10 +1,10 @@
-export const getAllContacts = ({ contacts }) => contacts;
+export const getAllContacts = ({ contacts }) => contacts.items;
 export const getFilteredContacts = ({ contacts, filter }) => {
   if (!filter) {
-    return contacts;
+    return contacts.items;
   }
 
-  return contacts.filter(contact =>
+  return contacts.items.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 };
