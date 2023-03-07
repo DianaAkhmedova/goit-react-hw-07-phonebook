11d1e23266ae73bd63c7a1fcf3af665003fa2ac4
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-03-08 00:13:34 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,21 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { getAllContacts } from 'redux/contacts/contacts-selectors';
-import { addContact } from 'redux/contacts/contacts-slice';
+import { fetchAddContact } from 'redux/contacts/contacts-operations';
 
 import { Form, FormLabel, FormInput, AddBtn } from './ContactForm.styled';
 
 const ContactForm = () => {
   const [state, setState] = useState({
     name: '',
-    number: '',
+    phone: '',
   });
 
-  const { name, number } = state;
-  const contacts = useSelector(getAllContacts);
+  const { name, phone } = state;
   const dispatch = useDispatch();
 
   const handleAddContact = contact => {
-    const isContainsContact = contacts.some(
-      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
-    );
-
-    if (isContainsContact)
-      return alert(`${contact.name} is already in contacts`);
-
-    dispatch(addContact(contact));
+    dispatch(fetchAddContact(contact));
   };
 
   const handleChange = ({ currentTarget }) => {
@@ -38,7 +29,7 @@ const ContactForm = () => {
   const handleSubmit = event => {
     event.preventDefault();
     handleAddContact({ ...state });
-    setState({ name: '', number: '' });
+    setState({ name: '', phone: '' });
   };
 
   return (
@@ -62,8 +53,8 @@ const ContactForm = () => {
         <FormInput
           onChange={handleChange}
           type="tel"
-          name="number"
-          value={number}
+          name="phone"
+          value={phone}
           placeholder="111-11-11"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
